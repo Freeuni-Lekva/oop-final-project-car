1844747af5ef71dbaea1c62f0914387b9830a678
sababa27 <saba.prangulashvili@gmail.com>
2025-06-23 23:50:37 +0400

    dzalian basic chat davamate

---
+++ src/main/java/org/example/car/AIAssistant/AIServlet.java
@@ -0,0 +1,37 @@
+package org.example.car.AIAssistant;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/AIServlet")
+public class AIServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String message = request.getParameter("message");
+        String AIresponse = message;
+
+        HttpSession session = request.getSession();
+
+        List<String> chat = (List<String>) session.getAttribute("chat");
+        if (chat == null) {
+            chat = new ArrayList<>();
+        }
+
+        chat.add("you: " + message);
+        chat.add("AI: " + AIresponse);
+
+        session.setAttribute("chat", chat);
+
+        response.sendRedirect("AIchat.jsp");
+
+
+    }
+}
