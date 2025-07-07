283bb7f93313868047dfd7eba1c074ce5f48d3c8
teklala <tvada22@freeuni.edu.ge>
2025-07-07 18:50:43 +0400

    user repository getUsers da deleteUser administvis

---
+++ src/main/java/org/example/car/User/Controller/AdminServlet.java
@@ -0,0 +1,16 @@
+package org.example.car.User.Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.car.BookingSystem.Repository.BookingRepository;
+import org.example.car.Car.Repository.CarRepository;
+import org.example.car.ReviewRepository;
+import org.example.car.User.Repository.UserRepository;
+
+@WebServlet("/admin")
+public class AdminServlet extends HttpServlet {
+
+}
--- src/main/java/org/example/car/User/Controller/UserProfileController.java
+++
@@ -1,5 +0,0 @@
-package org.example.car.User.Controller;
-
-public class UserProfileController {
-
-}
