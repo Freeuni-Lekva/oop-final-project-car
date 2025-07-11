453b2f8c5ad3784a94b5c4e7890ab2bab23d55bf
teklala <tvada22@freeuni.edu.ge>
2025-07-11 13:50:53 +0400

    AdminServlet da admin-dashboard.jsp davwere

--- src/main/java/org/example/car/User/Controller/AdminServlet.java
+++ src/main/java/org/example/car/User/Controller/AdminServlet.java
@@ -1,10 +1,34 @@
 package org.example.car.User.Controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.example.car.User.Model.User;
+import org.example.car.User.Repository.UserRepository;
+
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/admin-dashboard")
 public class AdminServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
 
+        if (user == null || !user.is_admin()) {
+            resp.sendRedirect(req.getContextPath() + "/Authentication/jsp/login.jsp?msg=Unauthorized+access");
+            return;
+        }
+
+        List<User> allUsers = UserRepository.getAllUsers();
+        req.setAttribute("admin", user);
+        req.setAttribute("users", allUsers);
+
+        req.getRequestDispatcher("/Admin/JSP/admin-dashboard.jsp").forward(req, resp);
+    }
 }
